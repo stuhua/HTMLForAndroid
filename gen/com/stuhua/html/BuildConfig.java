14ce39b5e4c4347c04c01e618a909266acc9f8c7
stuhua <qq850360626@gmail.com>
2016-08-04 10:45:40 +0800

    commit first

---
+++ gen/com/stuhua/html/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.stuhua.html;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/stuhua/html/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stuhua.html;
+
+import com.phonegap.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+}
